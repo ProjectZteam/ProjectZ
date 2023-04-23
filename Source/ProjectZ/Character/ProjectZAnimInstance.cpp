384f04fae68b8acd21a0d785e0075804293f9d8b
NoTitec <zkxkr0@gmail.com>
2023-04-23 11:30:52 +0900

    Enhanced Input 입력 기반 캐릭터 이동 구현, 사용할 애니메이션들 리타겟팅 이용하여 제작한 애셋정리폴더 구글드라이브

---
+++ Source/ProjectZ/Character/ProjectZAnimInstance.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectZAnimInstance.h"
+#include "ProjectZCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+void UProjectZAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	ProjectZCharacter=Cast<AProjectZCharacter>(TryGetPawnOwner());
+
+}
+
+void UProjectZAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (ProjectZCharacter == nullptr)
+	{
+		ProjectZCharacter = Cast<AProjectZCharacter>(TryGetPawnOwner());
+	}
+	if (ProjectZCharacter == nullptr) return;
+
+	FVector Velocity = ProjectZCharacter->GetVelocity();
+	Velocity.Z = 0.f;
+	Speed = Velocity.Size();
+
+	bIsInAir = ProjectZCharacter->GetCharacterMovement()->IsFalling();
+
+	bIsAccelerating = ProjectZCharacter->GetCharacterMovement()->GetCurrentAcceleration().Size() > 0.f ? true:false;
+}
